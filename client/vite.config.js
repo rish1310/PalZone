d0356a91c26bfc5e22f4b0e0663f0482fd612ad2
Rishabh Tripathi <devr1310@gmail.com>
2024-05-23 12:42:20 +0530

    pwa

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,6 +2,40 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+import { VitePWA } from 'vite-plugin-pwa';
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: 'Palzone',
+        short_name: 'Palzone',
+        description: 'A vibrant social media platform.',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/icons/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icons/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: '/icons/maskable_icon.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ]
+      },
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      }
+    })
+  ]
+});
